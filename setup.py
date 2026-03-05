dd0db9d3d66ffaeb83fdd8836dc3ef63ef708e10
DVNecromancer <97798602+DVNecromancer@users.noreply.github.com>
2026-03-05 13:51:35 +0000

    Update setup.py
    no torchtext version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="annalog",
-    version="0.5",
+    version="0.6",
     packages=find_packages(), 
     install_requires=[
         "partialsmiles @ git+https://github.com/baoilleach/partialsmiles.git"
